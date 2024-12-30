c61f4dda8bf9a942d4417a657eb6f06689f8823e
r-sei <ryousei3964@gmail.com>
2024-12-30 21:46:52 +0900

    ハリボテAppBarのUI作成

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -43,9 +43,32 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        centerTitle: false,
+        backgroundColor: Colors.white,
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text("Hello I'm",
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 12.0,
+                )),
+            Text("Mohamed Ali",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 15.0,
+                )),
+          ],
+        ),
+        // const Text("Hello I'm"),
+        leading: _ProfileIcon(),
+        actions: [
+          IconButton(onPressed: null, icon: Icon(Icons.calendar_month)),
+          IconButton(
+              onPressed: null, icon: const Icon(Icons.notifications_none))
+        ],
       ),
+
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -65,6 +88,17 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget _ProfileIcon() {
+    return IconButton(
+      onPressed: null,
+      icon: CircleAvatar(
+        backgroundColor: const Color.fromRGBO(247, 236, 21, 1),
+        child: Image.asset('assets/profile.png'),
+        radius: 30.0,
+      ),
     );
   }
 }
