604dff5e10b3dd5280e013b727f5797dc3c574f5
r-sei <ryousei3964@gmail.com>
2025-02-10 16:25:12 +0900

    📝 riverpod追加

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_practice/components/todo_modal.dart';
 import 'package:flutter_practice/components/todo_tile.dart';
 import 'package:flutter_practice/firebase_options.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:gap/gap.dart';
@@ -12,7 +13,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(ProviderScope(child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
